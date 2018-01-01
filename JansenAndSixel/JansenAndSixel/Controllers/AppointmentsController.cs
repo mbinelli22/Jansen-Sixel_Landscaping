6eb64f9fe175de3a653bd30e7d23eaeca436a08b
mbinelli22 <mbinelli1996@gmail.com>
2018-01-01 17:45:40 -0600

    PhotoGalleries page. can upload photo

--- JansenAndSixel/JansenAndSixel/Controllers/AppointmentsController.cs
+++ JansenAndSixel/JansenAndSixel/Controllers/AppointmentsController.cs
@@ -48,13 +48,40 @@ namespace JansenAndSixel.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Address,ZipCode,Date,Time,ProjectType")] Appointment appointment)
+        public async Task<ActionResult> Create([Bind(Include = "Id,FirstName,LastName,Address,ZipCode,Date,Time,ProjectType")] Appointment appointment)
         {
             if (ModelState.IsValid)
             {
+
                 db.Appointments.Add(appointment);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("mbinelli1996@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("michaeljordan@gmail.com");  // replace with valid value
+                message.Subject = "New Appointment Request";
+                message.Body = string.Format(body, appointment.FirstName, appointment.LastName, appointment.FromEmail, appointment.Address, appointment.PhoneNumber, appointment.ZipCode, appointment.Date, appointment.Time, appointment.ProjectType, appointment.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "michaeljordan@gmail.com",  // replace with valid value
+                        Password = "Password2!"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+
+
+
+                    //return RedirectToAction("Index");
+                }
             }
 
             return View(appointment);
